d5d9c2b61da6b35862c33168d076011d91d9e072
Alex Bepple <alex@bepple.de>
2011-08-01 01:16:21 +0200

    Remove superfluous menu items

--- lib/application.rb
+++ lib/application.rb
@@ -14,29 +14,6 @@ class Moriarty
     end
   end
 
-  # file/open
-  def on_open(menu)
-  end
-
-  # file/new
-  def on_new(menu)
-  end
-
-  # help menu item
-  def on_help(menu)
-  end
-
-  # This is commented out, so the minimize menu item is disabled
-  #def on_minimize(menu)
-  #end
-
-  # window/zoom
-  def on_zoom(menu)
-  end
-
-  # window/bring_all_to_front
-  def on_bring_all_to_front(menu)
-  end
 end
 
 Moriarty.new.start
