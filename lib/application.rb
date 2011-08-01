d1d834b6bea49d04a9ab036947cb3a1b350cc20f
Alex Bepple <alex@bepple.de>
2011-08-01 02:19:50 +0200

    Rought layout

--- lib/application.rb
+++ lib/application.rb
@@ -7,9 +7,42 @@ class Moriarty
   def start
     application :name => "Moriarty" do |app|
       app.delegate = self
-      window :frame => [100, 100, 500, 500], :title => "Moriarty" do |win|
-        win << label(:text => "Hello from HotCocoa", :layout => {:start => false})
+      window :size => [500, 500], :center => true, :view => :nolayout, :title => "Moriarty" do |win|
         win.will_close { exit }
+
+        win.view = layout_view(:layout => {
+            :expand => [:width, :height],
+            :padding => 0, :margin => 0
+        }) do |vert|
+            vert << layout_view(
+                :frame => [0, 0, 0, 40],
+                :layout => {
+                    :expand => [:width],
+                    :start => false,
+                },
+            ) do |frame|
+                frame << text_field(:layout => {
+                    :expand => [:width],
+                })
+            end
+
+            vert << scroll_view(
+                :layout => {:expand => [:width, :height]}
+            ) do |scroll|
+                scroll << table_view(
+                    :columns => [
+                        column(:id => :name, :title => 'Name'),
+                        column(:id => :location, :title => 'Location'), 
+                    ], 
+                    :data => [
+                        {:name => 'README.md', :location => '/usr/local'},
+                        {:name => 'git-completion.bash', :location => '/usr/local/etc/bash_completion.d'},
+                    ]
+                ) do |table|
+                    table.setUsesAlternatingRowBackgroundColors(true)
+                end
+            end
+        end
       end
     end
   end
